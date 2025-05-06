a825385228c455bca000e0b5794e70dea102ddbf
kajxcode <kaj@gmail.com>
2025-05-06 18:45:54 +1000

    fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str= "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find(str[39:45]):])
+print(str[str.find("object"):])
