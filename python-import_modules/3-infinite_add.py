65db875706ac38d5cbf6f7ec72bf8b70ef18e693
kajxcode <kaj@gmail.com>
2025-05-11 15:39:58 +1000

    fx

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == "__main__":
     total = 0
     for i in range(1, len(sys.argv)):
-        total=+ int(sys.argv[i])
+        total += int(sys.argv[i])
         print("{}".format(total))
