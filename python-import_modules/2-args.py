b0194dfdce821ce59e7268d60b2edc1e0d96b420
kajxcode <kaj@gmail.com>
2025-05-11 15:33:21 +1000

    fx

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-count = len(sys.argv) - 1
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
 if count == 0:
     print("0 arguments.")
 else:
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-total = 0
-for i in range(1, (sys.argv)):
-    total=+ int(sys.argv[i])
-    print("{}".format(total))
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, (sys.argv)):
+        total=+ int(sys.argv[i])
+        print("{}".format(total))
