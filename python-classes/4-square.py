ca7ff37c7523389f50bc22238538884de524b255
kajxcode <kaj@gmail.com>
2025-05-21 22:27:28 +1000

    fx

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""this module defines a class"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """intialise a new square
+
+        args:
+        size : size of square
+        """
+    def size(self, value):
+        """sets the size of square
+        args: value int 
+        raises: type error, value error
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """defines area of square"""
+        return self.__size ** 2
