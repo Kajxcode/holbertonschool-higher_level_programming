c62b6621cfa76901cd3707bab12139a46871e810
kajxcode <kaj@gmail.com>
2025-05-21 23:40:23 +1000

    fx

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -33,3 +33,11 @@ class Square:
     def area(self):
         """defines area of square"""
         return self.__size ** 2
+    
+    def my_print(self):
+        """print square with #"""
+        if self.__size is 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
