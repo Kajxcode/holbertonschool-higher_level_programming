d2ff04468c82570784a92c86f1604974a703ddf3
kajxcode <kaj@gmail.com>
2025-05-21 17:40:23 +1000

    fx

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """defines a square"""
     pass
-    def __init__(self, size):
+    def __init__(self, size=0):
         """intialise a new square
 
         args:
@@ -13,7 +13,7 @@ class Square:
         """
         self.__size = size
 
-        if size is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size >= 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
