420d7fd436bddb2bcd01c5bc53352cfcd91f3c13
kajxcode <kaj@gmail.com>
2025-05-21 17:26:37 +1000

    add

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module defines a class"""
+
+
+class Square:
+    """defines a square"""
+    pass
+def __init__(self, size):
+    """intialise a new square
+    
+    args:
+    size : size of square
+    """
+    self.size__size = size
+    
---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this module defines a class"""
+
+
+class Square:
+    """defines a square"""
+    pass
+def __init__(self, size=0):
+    """intialise a new square
+    
+    args:
+    size : size of square
+    """
+    self.size__size = size
+    
+    if size is not int:
+        raise TypeError("size must be an integer")
+    if size >= 0:
+        raise ValueError("size must be >= 0")
