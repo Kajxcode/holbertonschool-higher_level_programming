3d58b0b9dac73339d01edab9a04564d6cc89baea
kajxcode <kaj@gmail.com>
2025-05-21 15:21:59 +1000

    fx

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,13 +6,7 @@ This module defines a function
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each '.', '?' or ':'.
-
-    Args:
-        text (str): The input text.
-
-    Raises:
-        TypeError: If text is not a string.
+    Prints a text with 2 new lines after each occurrence of '.', '?', and ':'.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
