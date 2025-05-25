24a110be92f3d5a724401fdf9b210fdd9b133a2c
kajxcode <kaj@gmail.com>
2025-05-25 15:26:17 +1000

    fx

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-"""this module defines a class"""
+"""This module defines a Rectangle class."""
 
 
 class Rectangle:
-    """defines a class called rectangle"""
-    pass
+    """Defines a class called Rectangle."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-         """retrieve width"""
-         return self.__width
+        """Retrieve width."""
+        return self.__width
 
     @width.setter
     def width(self, value):
-         """sets width"""
-         if not isinstance(value, int):
+        """Set width with validation."""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-         if value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-         self.__width = value
-    
+        self.__width = value
+
     @property
     def height(self):
-        """retireves height"""
+        """Retrieve height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height"""
+        """Set height with validation."""
         if not isinstance(value, int):
-                raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-                raise ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-"""this module defines a class"""
+"""This module defines a Rectangle class."""
 
 
 class Rectangle:
-    """defines a class called rectangle"""
-    pass
+    """Defines a class called Rectangle."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-         """retrieve width"""
-         return self.__width
+        """Retrieve width."""
+        return self.__width
 
     @width.setter
     def width(self, value):
-         """sets width"""
-         if not isinstance(value, int):
+        """Set width with validation."""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-         if value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-         self.__width = value
-    
+        self.__width = value
+
     @property
     def height(self):
-        """retireves height"""
+        """Retrieve height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height"""
+        """Set height with validation."""
         if not isinstance(value, int):
-                raise TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-                raise ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     def area(self):
-         return self.__height * self.__width
-    
+        """Return the area of the rectangle."""
+        return self.__height * self.__width
+
     def perimeter(self):
-         if self.__width or self.__height is 0:
-              return 0
-         return 2 * (self.__height + self.__width)
+        """Return the perimeter, or 0 if width or height is 0."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
