5a6957b044c15775846147ac53cf3c61bd7e0d40
kajxcode <kaj@gmail.com>
2025-05-30 12:30:54 +1000

    fzx

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,8 +9,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle inherits area() behavior and integer_validator()
  from BaseGeometry"""
-
-
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
