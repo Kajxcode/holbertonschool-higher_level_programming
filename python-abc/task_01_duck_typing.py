7a06f144f23fbd15f9631d2e0edbfcc3b1dfdb6f
kajxcode <kaj@gmail.com>
2025-06-01 18:32:09 +1000

    fx

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""this module defines a class"""
+
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+import math 
+
+class Circle(Shape):
+    def __init__(self, radius=0):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter", shape.perimeter())
