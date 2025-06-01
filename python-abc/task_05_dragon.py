e7c07c75f6e0765ea584909c02ecd61cd110510a
kajxcode <kaj@gmail.com>
2025-06-02 01:31:48 +1000

    fx

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this class defines a module"""
+
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
