3e02bf439fb7bbdab370cc361a8d601f2d7d5d7c
kajxcode <kaj@gmail.com>
2025-06-12 14:46:38 +1000

    fx

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(HTTPStatus.NOT_FOUND)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Error: Endpoint not found")
+            self.wfile.write(b"404 Not Found: Endpoint not found")
 
 if __name__ == '__main__':
     PORT = 8000
