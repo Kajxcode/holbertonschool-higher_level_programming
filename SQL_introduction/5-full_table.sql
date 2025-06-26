8ac7f2b8f414277bf6a2402c80bc87fd3be04659
kajxcode <kaj@gmail.com>
2025-06-26 12:45:17 +1000

    fx

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
 -- script that prints description of table
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
-
-SHOW CREATE TABLE first_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
+SHOW CREATE TABLE first_table
