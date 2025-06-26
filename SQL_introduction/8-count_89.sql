b213c4e29e44960d52b642e9b39a2991dd685549
kajxcode <kaj@gmail.com>
2025-06-26 13:53:59 +1000

    fx

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- script that finds all records with x
-SELECT * FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE second_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    score INT
+);
+
+INSERT INTO first_table (id, name, score)
+VALUES
+(1, 'John', 10)
+(2, 'Alex', 3)
+(3, 'Bob', 14)
+(4, 'George', 8);
