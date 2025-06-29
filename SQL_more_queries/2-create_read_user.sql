ab65925f0ce1517e7b1461946741b929ce7c2dbc
kajxcode <kaj@gmail.com>
2025-06-29 11:45:22 +1000

    fx

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- script that creates a new user with select privileges
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
