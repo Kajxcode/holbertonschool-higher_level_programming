9e336234d2862de7f9dd77a064e11f76f70a42a0
kajxcode <kaj@gmail.com>
2025-06-29 13:19:30 +1000

    fx

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- script that lists cities of california
 SELECT cities.id, cities name
 FROM cities
-WHERE cities.stae.id = (
+WHERE cities.state.id = (
     SELECT id FROM states WHERE name = 'California' LIMIT 1
 )
 ORDER BY cities.id ASC;
