2d937557fd80ed7b1e66c942b45ce851604a67e5
kajxcode <kaj@gmail.com>
2025-06-29 14:20:15 +1000

    fx

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- script that imports database and lists ids
+SELECT genre.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genre.name
+ORDER BY number_of_shows DESC;
