4f38e5825c8342815b8611604ead7636a89d0157
kajxcode <kaj@gmail.com>
2025-06-29 15:17:57 +1000

    fx

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,7 @@
 -- script that imports database and lists dexter genre id
 SELECT tv_genres.name
-FROM tv_genres
-WHERE tv_genres.id IN (
-    SELECT tv_show_genres.genre_id
-    FROM tv_show_genres
-    WHERE tv_show_genres.show_id = (
-        SELECT id FROM tv_shows WHERE title = 'Dexter' LIMIT 1
-    )
-)
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
