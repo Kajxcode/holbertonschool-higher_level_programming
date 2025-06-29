d4115114b0a4854f8d43e343d58c5f2acba7aef2
kajxcode <kaj@gmail.com>
2025-06-29 13:01:42 +1000

    fx

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates a database and table
 CREATE DATABASE hbtn_0d_usa;
 CREATE TABLE states (
-    id INT UNIQUE PRIMARY KEY NOT NULL,
+    id INT UNIQUE, AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(256) NOT NULL
 );
