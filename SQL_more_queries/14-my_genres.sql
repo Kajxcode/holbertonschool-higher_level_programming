0566675ecb3f9a8b70c834a4a655f9478615854c
kajxcode <kaj@gmail.com>
2025-06-29 15:11:54 +1000

    fx

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,1 +1,11 @@
--- scr
+-- script that imports database and lists ids
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id IN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    WHERE tv_show_genres.show_id = (
+        SELECT id from tv_shows WHERE title = 'Dexter' LIMIT 1
+    )
+)
+ORDER BY tv_genres.name ASC;
