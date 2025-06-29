8e65305bde18c59832009dd7bea31457b9ee8eb6
kajxcode <kaj@gmail.com>
2025-06-29 11:47:59 +1000

    fx

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- script that creates a user with grants
+CREATE DATABASE hbtn_0d_2;
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
