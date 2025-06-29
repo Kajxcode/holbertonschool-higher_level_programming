7eb4c68dcf6ea3c16237a8cc064f96a956ce9c60
kajxcode <kaj@gmail.com>
2025-06-29 11:35:30 +1000

    fx

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates a user with grants
-CREATE USER 'user_0d_1'@'host' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
