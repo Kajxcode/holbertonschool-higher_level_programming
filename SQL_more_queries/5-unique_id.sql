094c7fe720c132d354228d581d45fb80c8c6ce2e
kajxcode <kaj@gmail.com>
2025-06-29 12:59:44 +1000

    fx

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- script creates a table
+CREATE TABLE unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates a database and table
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE states (
+    id INT UNIQUE PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
