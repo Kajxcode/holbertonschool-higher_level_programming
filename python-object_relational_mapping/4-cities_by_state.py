c95b887bfee54fb4ca78b316f4c29d3db59aed79
kajxcode <kaj@gmail.com>
2025-07-05 15:00:10 +1000

    fx

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name"
+        "SELECT cities.id, cities.name, states.name "
         "FROM cities "
-        "JOIN states ON cities.state_id = states.id"
+        "JOIN states ON cities.state_id = states.id "
         "ORDER BY cities.id ASC"
         )
     rows = cur.fetchall()
