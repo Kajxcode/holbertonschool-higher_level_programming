2f8b6c49746bec645cd30ae400eb5570ade9ed3f
kajxcode <kaj@gmail.com>
2025-07-05 15:45:37 +1000

    fx

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+defines state class mapped to states table in mysql using sqlalchemy
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """state class mapped to states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    USER = 'root'
+    PASSWORD = 'root'
+    DB_NAME = 'hbtn_0e_0_usa'
+    engine = create_engine(f'mysql+mysqldb://{USER}:{PASSWORD}@localhost:3306/{DB_NAME}',
+                            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
