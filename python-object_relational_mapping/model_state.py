e158d867aeed28ce341ebcf2cd9f8466cdfdbdae
kajxcode <kaj@gmail.com>
2025-07-05 15:47:32 +1000

    fx

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """state class mapped to states table"""
     __tablename__ = 'states'
@@ -15,11 +16,14 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
     USER = 'root'
     PASSWORD = 'root'
     DB_NAME = 'hbtn_0e_0_usa'
-    engine = create_engine(f'mysql+mysqldb://{USER}:{PASSWORD}@localhost:3306/{DB_NAME}',
-                            pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{USER}:{PASSWORD}@localhost:3306/{DB_NAME}',
+        pool_pre_ping=True
+        )
 
     Base.metadata.create_all(engine)
