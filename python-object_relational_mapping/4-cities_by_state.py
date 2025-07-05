15690cdcc198b022378557555e3a1e001b367aa1
kajxcode <kaj@gmail.com>
2025-07-05 14:54:29 +1000

    fx

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that displays all cities from db
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db_name
+
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM cities ORDER BY id ASC"
+        )
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    
+    cur.close()
+    db.close()
