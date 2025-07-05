a486f100d6ed1fcbf87b0931e3213bbad5b52c1c
kajxcode <kaj@gmail.com>
2025-07-05 15:07:20 +1000

    fx

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+script that takes an argument and displays all values that match
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db_name
+
+    )
+    cur = db.cursor()
+    query = (
+        "SELECT * FROM states WHERE BINARY name = %s "
+        "ORDER BY id ASC"
+    )
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (state_searched,)
+    )
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
