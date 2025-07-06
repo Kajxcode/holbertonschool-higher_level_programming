003f2ba89c932d99228060ecf67ead586eeedcec
kajxcode <kaj@gmail.com>
2025-07-06 16:10:37 +1000

    fx

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,6 +10,10 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     user = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
