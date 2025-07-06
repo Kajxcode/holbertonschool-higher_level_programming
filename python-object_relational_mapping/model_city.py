01b0fe05b7a4cebb9b980809236534f364bc4b14
kajxcode <kaj@gmail.com>
2025-07-06 14:10:19 +1000

    fx

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+defines a city class mapped to cities table in mysql using alchemy
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """city class mapped to cities table"""
+    ___tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+
+if __name__ == "__main__":
+    USER = 'root'
+    PASSWORD = 'root'
+    DB_NAME = 'hbtn_0e_0_usa'
+    engine = create_engine(
+        f'mysql+mysqldb://{USER}:{PASSWORD}@localhost:3306/{DB_NAME}',
+        pool_pre_ping=True
+        )
+
+    Base.metadata.create_all(engine)
