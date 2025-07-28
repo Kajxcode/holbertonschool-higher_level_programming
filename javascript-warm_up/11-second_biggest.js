efe329d8fd6e2218085a070f49a4302097265847
kajxcode <kaj@gmail.com>
2025-07-28 14:02:13 +1000

    fx

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = proccess.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log (0);
+} else {
+  const uniqueArgs = [...new Set(args)];
+  if (unqiueArgs.length <= 1) {
+    console.log(0);
+  } else {
+    unqiueArgs.sort((a, b) => b - a);
+    console.log(uniqueArgs[1]);
+  }
+}
