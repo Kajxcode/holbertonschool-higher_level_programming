25eebf25c078fbc82b80abc7bd52f0b76737dc40
kajxcode <kaj@gmail.com>
2025-07-28 12:56:46 +1000

    fx

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+
 const arg = process.argv[2];
 const num = Number(arg);
 
 if (!isNaN(num) && Number.isInteger(num)) {
     console.log(`My number: ${num}`);
-} else {
+}   else {
     console.log('Not a number');
 }
